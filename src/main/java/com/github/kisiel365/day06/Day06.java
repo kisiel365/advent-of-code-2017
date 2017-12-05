da29fa63d082c95d44dcb9438d519054a224026d
tznl <tznl@PLLL0158.gft.com>
2017-12-05 17:36:24 +0100

    Prepare for day 6 riddle

---
+++ src/main/java/com/github/kisiel365/day06/Day06.java
@@ -0,0 +1,8 @@
+package com.github.kisiel365.day06;
+
+public final class Day06 {
+
+	private Day06() {
+	}
+
+}
---
+++ src/test/java/com/github/kisiel365/day06/Day06Test.java
@@ -0,0 +1,8 @@
+package com.github.kisiel365.day06;
+
+import util.TestUtil;
+
+public class Day06Test {
+	private static final String CASE_INPUT = TestUtil.loadResourceAsString("day06/caseInput");
+
+}
