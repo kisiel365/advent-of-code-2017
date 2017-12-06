fa86ab61642d42ab96c692a46ad5cfafdbc5c6cb
tznl <tznl@PLLL0158.gft.com>
2017-12-06 12:08:52 +0100

    Solve day 6 riddle

--- src/main/java/com/github/kisiel365/day06/Day06.java
+++ src/main/java/com/github/kisiel365/day06/Day06.java
@@ -1,8 +1,66 @@
 package com.github.kisiel365.day06;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public final class Day06 {
 
 	private Day06() {
+	}
+
+	public static int loopEnd(String input) {
+		Integer[] banks = parseInput(input);
+		int reallocateSteps = 0;
+		Set<String> previousStates = new HashSet<>();
+		while (previousStates.add(Arrays.toString(banks))) {
+			reallocate(banks);
+			reallocateSteps++;
+		}
+		return reallocateSteps;
+	}
+
+	public static int loopSize(String input) {
+		Integer[] banks = parseInput(input);
+		int reallocateSteps = 0;
+		List<String> previousStates = new ArrayList<>();
+		while (!previousStates.contains(Arrays.toString(banks))) {
+			previousStates.add(Arrays.toString(banks));
+			reallocate(banks);
+			reallocateSteps++;
+		}
+		int beginingOfCycle = previousStates.indexOf(Arrays.toString(banks));
+		return reallocateSteps - beginingOfCycle;
+	}
+
+	private static Integer[] parseInput(String input) {
+		String[] split = input.split("\t");
+		Integer[] banks = new Integer[split.length];
+		for (int i = 0; i < banks.length; i++)
+			banks[i] = Integer.parseInt(split[i]);
+		return banks;
+	}
+
+	private static void reallocate(Integer[] banks) {
+		int indexOfMax = indexOfMax(banks);
+		int toRellocate = banks[indexOfMax];
+		int targetRelocation = (indexOfMax + 1) % banks.length;
+		banks[indexOfMax] = 0;
+		while (toRellocate > 0) {
+			banks[targetRelocation]++;
+			targetRelocation = (targetRelocation + 1) % banks.length;
+			toRellocate--;
+		}
+	}
+
+	private static int indexOfMax(Integer[] banks) {
+		int index = 0;
+		for (int i = 1; i < banks.length; i++)
+			if (banks[i] > banks[index])
+				index = i;
+		return index;
 	}
 
 }
--- src/test/java/com/github/kisiel365/day06/Day06Test.java
+++ src/test/java/com/github/kisiel365/day06/Day06Test.java
@@ -1,8 +1,32 @@
 package com.github.kisiel365.day06;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import util.TestUtil;
 
 public class Day06Test {
 	private static final String CASE_INPUT = TestUtil.loadResourceAsString("day06/caseInput");
+
+	@Test
+	public void simpleExample() {
+		assertEquals(5, Day06.loopEnd("0\t2\t7\t0"));
+	}
+
+	@Test
+	public void simpleCase() {
+		assertEquals(6681, Day06.loopEnd(CASE_INPUT));
+	}
+
+	@Test
+	public void advancedExample() {
+		assertEquals(4, Day06.loopSize("0\t2\t7\t0"));
+	}
+
+	@Test
+	public void advancedCase() {
+		assertEquals(2392, Day06.loopSize(CASE_INPUT));
+	}
 
 }
