a022b1277f5ac3c3950883c95c92979dca524983
tznl <tznl@PLLL0158.gft.com>
2017-12-08 15:18:58 +0100

    Refactor solution for day 8 riddle

--- src/main/java/com/github/kisiel365/day08/Command.java
+++ src/main/java/com/github/kisiel365/day08/Command.java
@@ -1,59 +1,47 @@
 package com.github.kisiel365.day08;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiPredicate;
 import java.util.regex.Matcher;
 
 public class Command {
-	String registerToModify;
-	String modification;
-	int modificationValue;
-	String registerForCondition;
-	String operator;
-	int valueToCompare;
+	private String registerToModify;
+	private int modificationValue;
+	private String registerForCondition;
+	private BiPredicate<Integer, Integer> predicate;
+	private int valueToCompare;
+
+	private static final Map<String, BiPredicate<Integer, Integer>> predicates = possiblePredicates();
 
 	Command(Matcher matcher) {
 		registerToModify = matcher.group(1);
-		modification = matcher.group(2);
-		modificationValue = Integer.parseInt(matcher.group(3));
+		modificationValue = "inc".equals(matcher.group(2)) ? Integer.parseInt(matcher.group(3))
+				: Integer.parseInt(matcher.group(3)) * -1;
 		registerForCondition = matcher.group(4);
-		operator = matcher.group(5);
+		predicate = predicates.get(matcher.group(5));
 		valueToCompare = Integer.parseInt(matcher.group(6));
 	}
 
-	@Override
-	public String toString() {
-		return "Command [registerToModify=" + registerToModify + ", modification=" + modification
-				+ ", modificationValue=" + modificationValue + ", registerForCondition=" + registerForCondition
-				+ ", operator=" + operator + ", valueToCompare=" + valueToCompare + "]";
+	private static final Map<String, BiPredicate<Integer, Integer>> possiblePredicates() {
+		Map<String, BiPredicate<Integer, Integer>> operators = new HashMap<>();
+		operators.put(">", (a, b) -> a > b);
+		operators.put("<", (a, b) -> a < b);
+		operators.put(">=", (a, b) -> a >= b);
+		operators.put("<=", (a, b) -> a <= b);
+		operators.put("==", (a, b) -> a.equals(b));
+		operators.put("!=", (a, b) -> !a.equals(b));
+		return operators;
 	}
 
 	public boolean fulfillsCondition(Map<String, Integer> registers) {
-		// too lazy for strategy / command pattern
 		Integer registerForConditionValue = registers.getOrDefault(registerForCondition, 0);
-		if (">".equals(operator)) {
-			return registerForConditionValue > valueToCompare ? true : false;
-		} else if ("<".equals(operator)) {
-			return registerForConditionValue < valueToCompare ? true : false;
-		} else if (">=".equals(operator)) {
-			return registerForConditionValue >= valueToCompare ? true : false;
-		} else if ("<=".equals(operator)) {
-			return registerForConditionValue <= valueToCompare ? true : false;
-		} else if ("==".equals(operator)) {
-			return registerForConditionValue == valueToCompare ? true : false;
-		} else if ("!=".equals(operator)) {
-			return registerForConditionValue != valueToCompare ? true : false;
-		}
-		throw new RuntimeException("Unknown operator: " + operator);
+		return predicate.test(registerForConditionValue, valueToCompare);
 	}
 
 	public void execute(Map<String, Integer> registers) {
 		Integer currentValue = registers.getOrDefault(registerToModify, 0);
-		if ("dec".equals(modification)) {
-			currentValue -= modificationValue;
-		} else if ("inc".equals(modification)) {
-			currentValue += modificationValue;
-		} else
-			throw new RuntimeException("Unknown modification: " + modification);
+		currentValue += modificationValue;
 		registers.put(registerToModify, currentValue);
 	}
 
