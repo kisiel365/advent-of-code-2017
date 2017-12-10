cbb41201d18b0e5431a4e831d473fcc71972e96c
programista <programista@programista-R780-R778>
2017-12-10 22:48:13 +0100

    Solve day 9 riddle

--- src/main/java/com/github/kisiel365/day09/Day09.java
+++ src/main/java/com/github/kisiel365/day09/Day09.java
@@ -5,12 +5,83 @@ public final class Day09 {
 	private Day09() {
 	}
 
-	public static int simple(String input) {
-		return 0;
+	public static int scoreGroups(String input) {
+		String escaped = escapeExclamationWithinGarbage(input);
+		String garbageSkipped = skipGarbage(escaped);
+		return scoreFilteredGroups(garbageSkipped);
 	}
 
-	public static int advanced(String input) {
-		return 0;
+	public static int countGarbage(String input) {
+		String escaped = escapeExclamationWithinGarbage(input);
+		return countGarbageWithinGroups(escaped);
+	}
+
+	private static int scoreFilteredGroups(String input) {
+		int score = 0;
+		int deepness = 0;
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			if (c == '{') {
+				score += ++deepness;
+			} else if (c == '}') {
+				deepness--;
+			}
+		}
+		return score;
+	}
+
+	private static String skipGarbage(String input) {
+		StringBuilder sb = new StringBuilder();
+		boolean withinGarbage = false;
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			if (!withinGarbage && c == '<')
+				withinGarbage = true;
+			else if (withinGarbage && c == '>')
+				withinGarbage = false;
+			else if (!withinGarbage)
+				sb.append(c);
+		}
+		return sb.toString();
+	}
+
+	private static String escapeExclamationWithinGarbage(String input) {
+		StringBuilder sb = new StringBuilder();
+		boolean withinGarbage = false;
+		boolean ignoreNext = false;
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			if (!ignoreNext) {
+				if (withinGarbage && c == '!') {
+					ignoreNext = true;
+				} else if (!withinGarbage && c == '<') {
+					withinGarbage = true;
+				} else if (withinGarbage && c == '>') {
+					withinGarbage = false;
+				}
+				if (!withinGarbage || c != '!')
+					sb.append(c);
+			} else {
+				ignoreNext = false;
+			}
+		}
+		return sb.toString();
+	}
+
+	private static int countGarbageWithinGroups(String input) {
+		boolean withinGarbage = false;
+		int garbageCount = 0;
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			if (!withinGarbage && c == '<')
+				withinGarbage = true;
+			else if (withinGarbage && c == '>')
+				withinGarbage = false;
+			else if (withinGarbage) {
+				garbageCount++;
+			}
+		}
+		return garbageCount;
 	}
 
 }
--- src/test/java/com/github/kisiel365/day09/Day09Test.java
+++ src/test/java/com/github/kisiel365/day09/Day09Test.java
@@ -2,7 +2,6 @@ package com.github.kisiel365.day09;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import util.TestUtil;
@@ -10,28 +9,37 @@ import util.TestUtil;
 public class Day09Test {
 	private static final String CASE_INPUT = TestUtil.loadResourceAsString("day09/caseInput");
 
-	@Ignore
 	@Test
 	public void simpleExample() {
-		assertEquals(0, Day09.simple(""));
+		assertEquals(1, Day09.scoreGroups("{}"));
+		assertEquals(6, Day09.scoreGroups("{{{}}}"));
+		assertEquals(5, Day09.scoreGroups("{{},{}}"));
+		assertEquals(16, Day09.scoreGroups("{{{},{},{{}}}}"));
+		assertEquals(1, Day09.scoreGroups("{<a>,<a>,<a>,<a>}"));
+		assertEquals(9, Day09.scoreGroups("{{<ab>},{<ab>},{<ab>},{<ab>}}"));
+		assertEquals(9, Day09.scoreGroups("{{<!!>},{<!!>},{<!!>},{<!!>}}"));
+		assertEquals(3, Day09.scoreGroups("{{<a!>},{<a!>},{<a!>},{<ab>}}"));
 	}
 
-	@Ignore
 	@Test
 	public void simpleCase() {
-		assertEquals(0, Day09.simple(CASE_INPUT));
+		assertEquals(17390, Day09.scoreGroups(CASE_INPUT));
 	}
 
-	@Ignore
 	@Test
 	public void advancedExample() {
-		assertEquals(0, Day09.advanced(""));
+		assertEquals(0, Day09.countGarbage("<>"));
+		assertEquals(17, Day09.countGarbage("<random characters>"));
+		assertEquals(3, Day09.countGarbage("<<<<>"));
+		assertEquals(2, Day09.countGarbage("<{!>}>"));
+		assertEquals(0, Day09.countGarbage("<!!>"));
+		assertEquals(0, Day09.countGarbage("<!!!>>"));
+		assertEquals(10, Day09.countGarbage("<{o\"i!a,<{i<a>"));
 	}
 
-	@Ignore
 	@Test
 	public void advancedCase() {
-		assertEquals(0, Day09.advanced(CASE_INPUT));
+		assertEquals(7825, Day09.countGarbage(CASE_INPUT));
 	}
 
 }
