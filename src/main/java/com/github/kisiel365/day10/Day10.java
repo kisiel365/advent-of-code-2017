6af3ebbe5441b688ddc5e1209ca52c3e903bb2e0
programista <programista@programista-R780-R778>
2017-12-11 00:07:26 +0100

    Solve first part of day 10 riddle

--- src/main/java/com/github/kisiel365/day10/Day10.java
+++ src/main/java/com/github/kisiel365/day10/Day10.java
@@ -1,15 +1,81 @@
 package com.github.kisiel365.day10;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.xml.bind.DatatypeConverter;
+
 public final class Day10 {
 
 	private Day10() {
 	}
 
-	public static int simple(String input) {
-		return 0;
+	public static int simple(int size, String input) {
+		int[] numbers = new int[size];
+		for (int i = 0; i < size; i++)
+			numbers[i] = i;
+		int currentPosition = 0;
+		int skipSize = 0;
+		List<Integer> lengths = Arrays.stream(input.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+		int timesMax = 1;
+		for (int times = 0; times < timesMax; times++)
+			for (int length : lengths) {
+				int reverseStart = currentPosition;
+				int reverseEnd = currentPosition + length - 1;
+				for (int i = 0; i < length / 2; i++) {
+					int tmp = numbers[(reverseStart + i + size) % size];
+					numbers[(reverseStart + i + size) % size] = numbers[(reverseEnd - i + size) % size];
+					numbers[(reverseEnd - i + size) % size] = tmp;
+				}
+				currentPosition += length + skipSize++;
+			}
+
+		return numbers[0] * numbers[1];
 	}
 
-	public static int advanced(String input) {
+	public static int toByte(int number) {
+		int tmp = number & 0xff;
+		return (tmp & 0x80) == 0 ? tmp : tmp - 256;
+	}
+
+	public static int advanced(String rawInput) {
+		List<Integer> asciified = rawInput.chars().boxed().collect(Collectors.toList());
+		asciified.addAll(Arrays.asList(17, 31, 73, 47, 23));
+		System.out.println(asciified);
+		int size = 256;
+		int[] numbers = new int[size];
+		for (int i = 0; i < size; i++)
+			numbers[i] = i;
+		int currentPosition = 0;
+		int skipSize = 0;
+		int timesMax = 1;
+		for (int times = 0; times < timesMax; times++)
+			for (int length : asciified) {
+				int reverseStart = currentPosition;
+				int reverseEnd = currentPosition + length - 1;
+				for (int i = 0; i < length / 2; i++) {
+					int tmp = numbers[(reverseStart + i + size) % size];
+					numbers[(reverseStart + i + size) % size] = numbers[(reverseEnd - i + size) % size];
+					numbers[(reverseEnd - i + size) % size] = tmp;
+				}
+				currentPosition += length + skipSize++;
+			}
+		System.out.println(Arrays.toString(numbers));
+
+		byte[] xored = new byte[16];
+		for (int block = 0; block < 16; block++) {
+			byte value = (byte) numbers[block * 16];
+			for (int numberInBlock = 1; numberInBlock < 16; numberInBlock++) {
+				value ^= numbers[block * 16 + numberInBlock];
+			}
+			xored[block] = value;
+		}
+		System.out.println(Arrays.toString(xored));
+
+		String printHexBinary = DatatypeConverter.printHexBinary(xored);
+		System.out.println(printHexBinary);
+
 		return 0;
 	}
 
--- src/test/java/com/github/kisiel365/day10/Day10Test.java
+++ src/test/java/com/github/kisiel365/day10/Day10Test.java
@@ -10,22 +10,23 @@ import util.TestUtil;
 public class Day10Test {
 	private static final String CASE_INPUT = TestUtil.loadResourceAsString("day10/caseInput");
 
-	@Ignore
 	@Test
 	public void simpleExample() {
-		assertEquals(0, Day10.simple(""));
+		assertEquals(12, Day10.simple(5, "3,4,1,5"));
 	}
 
-	@Ignore
 	@Test
 	public void simpleCase() {
-		assertEquals(0, Day10.simple(CASE_INPUT));
+		assertEquals(23715, Day10.simple(256, CASE_INPUT));
 	}
 
 	@Ignore
 	@Test
 	public void advancedExample() {
-		assertEquals(0, Day10.advanced(""));
+		assertEquals("a2582a3a0e66e6e86e3812dcb672a272", Day10.advanced(""));
+		assertEquals("33efeb34ea91902bb2f59c9920caa6cd", Day10.advanced("AoC 2017"));
+		assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", Day10.advanced("1,2,3"));
+		assertEquals("63960835bcdc130f0b66d7ff4f6a5a8e", Day10.advanced("1,2,4"));
 	}
 
 	@Ignore
