f1414f4a8a177451db255346df842ee0053ab187
tznl <tznl@PLLL0158.gft.com>
2017-12-15 15:31:29 +0100

    Solve day 15 riddle

--- src/main/java/com/github/kisiel365/day15/Day15.java
+++ src/main/java/com/github/kisiel365/day15/Day15.java
@@ -2,15 +2,51 @@ package com.github.kisiel365.day15;
 
 public final class Day15 {
 
+	private static final long GEN_A_FACTOR = 16807;
+	private static final long GEN_B_FACTOR = 48271;
+	private static final long DIVIDER = 2147483647;
+
 	private Day15() {
 	}
 
-	public static int simple(String input) {
-		return 0;
+	public static int simple(int generatorAStartingValue, int generatorBStartingValue) {
+		long genAvalue = generatorAStartingValue;
+		long genBvalue = generatorBStartingValue;
+		int ticks = 40000000;
+		int counter = 0;
+		for (int i = 0; i < ticks; i++) {
+			genAvalue = getNextValue(genAvalue, GEN_A_FACTOR);
+			genBvalue = getNextValue(genBvalue, GEN_B_FACTOR);
+			if (judge(genAvalue, genBvalue))
+				counter++;
+		}
+		return counter;
 	}
 
-	public static int advanced(String input) {
-		return 0;
+	private static long getNextValue(long genAvalue, long genAFactor) {
+		return (genAvalue * genAFactor) % DIVIDER;
+	}
+
+	public static boolean judge(long valueA, long valueB) {
+		return (valueA & 0xFFFF) == (valueB & 0xFFFF);
+	}
+
+	public static int advanced(int generatorAStartingValue, int generatorBStartingValue) {
+		long genAvalue = generatorAStartingValue;
+		long genBvalue = generatorBStartingValue;
+		int ticks = 5000000;
+		int counter = 0;
+		for (int i = 0; i < ticks; i++) {
+			do {
+				genAvalue = getNextValue(genAvalue, GEN_A_FACTOR);
+			} while ((genAvalue & 3) != 0); // modulo 4
+			do {
+				genBvalue = getNextValue(genBvalue, GEN_B_FACTOR);
+			} while ((genBvalue & 7) != 0); // modulo 8
+			if (judge(genAvalue, genBvalue))
+				counter++;
+		}
+		return counter;
 	}
 
 }
--- src/test/java/com/github/kisiel365/day15/Day15Test.java
+++ src/test/java/com/github/kisiel365/day15/Day15Test.java
@@ -1,37 +1,40 @@
 package com.github.kisiel365.day15;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-import util.TestUtil;
-
 public class Day15Test {
-	private static final String CASE_INPUT = TestUtil.loadResourceAsString("day15/caseInput");
 
-	@Ignore
 	@Test
 	public void simpleExample() {
-		assertEquals(0, Day15.simple(""));
+		assertEquals(588, Day15.simple(65, 8921));
 	}
 
-	@Ignore
 	@Test
 	public void simpleCase() {
-		assertEquals(0, Day15.simple(CASE_INPUT));
+		assertEquals(594, Day15.simple(703, 516));
 	}
 
-	@Ignore
+	@Test
+	public void judgeTest() {
+		assertTrue(Day15.judge(0, 0));
+		assertTrue(Day15.judge(65535, 65535));
+		assertFalse(Day15.judge(65535, 65534));
+		assertFalse(Day15.judge(65535, 32767));
+		assertTrue(Day15.judge(131071, 65535));
+	}
+
 	@Test
 	public void advancedExample() {
-		assertEquals(0, Day15.advanced(""));
+		assertEquals(309, Day15.advanced(65, 8921));
 	}
 
-	@Ignore
 	@Test
 	public void advancedCase() {
-		assertEquals(0, Day15.advanced(CASE_INPUT));
+		assertEquals(328, Day15.advanced(703, 516));
 	}
 
 }
