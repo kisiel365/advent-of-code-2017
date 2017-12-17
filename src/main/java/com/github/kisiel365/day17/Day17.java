4bdc0c087550738b75a82b832638c705ae81a1ad
programista <programista@programista-R780-R778>
2017-12-17 20:10:36 +0100

    Solve day 17 riddle

--- src/main/java/com/github/kisiel365/day17/Day17.java
+++ src/main/java/com/github/kisiel365/day17/Day17.java
@@ -1,16 +1,38 @@
 package com.github.kisiel365.day17;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Day17 {
 
 	private Day17() {
 	}
 
-	public static int simple(String input) {
-		return 0;
+	public static int firstValueAfter2017(int step) {
+		List<Integer> list = new ArrayList<>();
+		list.add(0);
+		int current = 0;
+		for (int i = 1; i <= 2017; i++) {
+			current = (current + step) % (list.size());
+			list.add(++current, i);
+		}
+		return list.get(list.indexOf(2017) + 1);
 	}
 
-	public static int advanced(String input) {
-		return 0;
+	public static int firstValueAfterZero(int step) {
+		int current = 0;
+		int zeroPosition = 0;
+		int listSize = 1;
+		int lastValueAfterZero = -1;
+		for (int i = 1; i <= 50000000; i++) {
+			current = (current + step) % (listSize) + 1;
+			listSize++;
+			if (current == zeroPosition + 1)
+				lastValueAfterZero = i;
+			if (current <= zeroPosition)
+				zeroPosition++;
+		}
+		return lastValueAfterZero;
 	}
 
 }
--- src/test/java/com/github/kisiel365/day17/Day17Test.java
+++ src/test/java/com/github/kisiel365/day17/Day17Test.java
@@ -2,36 +2,23 @@ package com.github.kisiel365.day17;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-import util.TestUtil;
-
 public class Day17Test {
-	private static final String CASE_INPUT = TestUtil.loadResourceAsString("day17/caseInput");
 
-	@Ignore
 	@Test
 	public void simpleExample() {
-		assertEquals(0, Day17.simple(""));
+		assertEquals(638, Day17.firstValueAfter2017(3));
 	}
 
-	@Ignore
 	@Test
 	public void simpleCase() {
-		assertEquals(0, Day17.simple(CASE_INPUT));
+		assertEquals(136, Day17.firstValueAfter2017(363));
 	}
 
-	@Ignore
-	@Test
-	public void advancedExample() {
-		assertEquals(0, Day17.advanced(""));
-	}
-
-	@Ignore
 	@Test
 	public void advancedCase() {
-		assertEquals(0, Day17.advanced(CASE_INPUT));
+		assertEquals(1080289, Day17.firstValueAfterZero(363));
 	}
 
 }
